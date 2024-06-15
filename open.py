6f590a8e37e6a5a627885b3b1b2a7834c7a5ab48
puredivine <168623923+puredivine@users.noreply.github.com>
2024-06-15 15:22:31 +0900

    Update open.py

--- open.py
+++ open.py
@@ -6,78 +6,87 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
 
-import keyboard
+from keyboard import wait as keyboard_wait
 from datetime import datetime
-from sys import exit as _exit
-from buildment import Buildment
-from time import sleep
+from sys import exit as sys_exit
+from traceback import format_exc
 
 
-DAY = ['20240623', '20240523', '20240629'][1]
-MAX_RSV = 1
-FLOOR = [1]
-SPOT = [12]
-SITE = ['{} 층 {} 구역'.format(FLOOR[k], SPOT[k]) for k in range(len(FLOOR))]
-
+DAY = ['20240803', '20240707'][0]
+TIME = 1
+TARGET = [
+    (2, 4),
+    (2, 5)
+]
+SITE = ['{} 층 {} 구역'.format(_set[0], _set[1]) for _set in TARGET]
 CAPTCHA_IMG = 'screenshot.png'
-BD = Buildment()
-SCIRPT = """
-var rects = document.getElementsByTagName('rect');
-var result = [];
-for (var i = 0; i < rects.length; i++) {
-    var rect = rects[i];
-    var color = rect.getAttribute("fill");
-    var width = rect.getAttribute("width");
-    var height = rect.getAttribute("height");
-    if (width == 11 && height == 11 && color !== '#DDDDDD') {
-        result.push(rect);
-    }
-}
-return result;
-"""
-
-
-
+_XPATH = '//*[@id="list_time"]/li[{}]/button/span/span'.format(TIME)
+TIME_NAME = '18시 00분'
 
 try:
-
-    print('wait for esc to run')
-    keyboard.wait('esc') # For test
-    print('running')
-
-
-
-    # main driver set up #
+    # Set Driver #
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--start-maximized")
     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
     macro_driver = webdriver.Chrome(options=chrome_options)
-    _wait = lambda sec: WebDriverWait(macro_driver, sec, poll_frequency=0.4)
+    _wait = lambda sec: WebDriverWait(macro_driver, sec, poll_frequency=0.1)
+
+    # macro_driver.set_page_load_timeout(0.5)
+    # Set Driver #
+
+
+
+    print('wait for esc to run')
+    keyboard_wait('esc') # For test
+    print('running')
+    start_time = datetime.now()
 
 
 
     # deal with the first window #
-    date = _wait(100).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#dateSelect_{} > button > span'.format(DAY))))
-    date.click()
-    _wait(10).until(EC.invisibility_of_element((By.CSS_SELECTOR, '#section_time > div > p')))
-    macro_driver.find_element(By.CSS_SELECTOR, '#ticketReservation_Btn').click()
+    btn_list = _wait(100).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#ticketing_process_box > div > div.box_ticketing_process > dl.date_choice > dd.sorting > button.type_list')))
+    btn_list.click()
+    btn_date = _wait(10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#dateSelect_{} > button > span'.format(DAY))))
+    btn_date.click()
+
+    
+    # while True:
+    #     k = macro_driver.find_element(By.XPATH, _XPATH).get_attribute('class')
+    #     print(k)
+    #     if DAY in k:
+    #         break
+    #     else: continue
+
+    btn_time = _wait(10).until(EC.all_of(
+        EC.text_to_be_present_in_element((By.XPATH, '//*[@id="list_time"]/li[{}]/button'.format(TIME)), TIME_NAME)),
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="list_time"]/li[{}]/button'.format(TIME))))
+    # btn_time.click()
+    btn_reservation = _wait(10).until(EC.element_to_be_clickable((By.ID,'ticketReservation_Btn')))
+    btn_reservation.click()
+    # deal with the first window #
 
 
 
     # deal with the second window #
     _wait(10).until(EC.number_of_windows_to_be(2))
-    macro_driver.switch_to.window(macro_driver.window_handles[1])
-    
-    _wait(10).until(lambda driver: driver.execute_script('return document.readyState;') == 'complete')
-    print('Pop-up Loaded', '\n')
-    
+    windows = macro_driver.window_handles
+    macro_driver.switch_to.window(windows[1])
+    print('new window')
+
+    _wait(10).until(lambda driver: driver.execute_script("return typeof jQuery != 'undefined'"))
+    _wait(10).until(lambda driver: driver.execute_script("return jQuery.active == 0"))
+
+    html = macro_driver.page_source
+    captcha_pres = False if html.find('id="certification"') == -1 else True
 
 
-    # search remain seats #
-    #keyboard.wait('esc')
-    print('After Captcha')
-    sleep(1) #################################### error 
+    print('cap', captcha_pres)
+    print('waiting')
+    keyboard_wait('esc')
+    print('second running')
+
+
+    # Reservation #
     macro_driver.switch_to.frame('oneStopFrame')
     
     area_opens = macro_driver.find_elements(By.CLASS_NAME, 'area_info')
@@ -87,48 +96,15 @@ try:
     residuals = macro_driver.find_elements(By.CLASS_NAME, 'seat_residual')
     site_len = len(site_names)
     
+    for i in range(site_len):
+        print(site_names[i], residuals[i].text)
+
+    # Reservation #
 
 
-    # Prior
-    seat_rsv_cnt = 0
-    for _target in SITE:
-        idx = site_names.index(_target)
-        if seat_rsv_cnt >= MAX_RSV: break
-        elif residuals[idx].text != '0석':
-            residuals[idx].click()
+except Exception:
+    print(format_exc())
 
-            seats = macro_driver.execute_script(SCIRPT)
-            seats[0].click()
-            seat_rsv_cnt += 1
-    
-    # Not Prior
-    if seat_rsv_cnt < MAX_RSV: 
-        for i in range(site_len):
-            if seat_rsv_cnt >= MAX_RSV: break
-            elif residuals[i].text != '0석':
-                residuals[i].click()
-
-                seats = macro_driver.execute_script(SCIRPT)
-                seats[0].click()
-                seat_rsv_cnt += 1
-                
-
-    
-    # Final Reservation #
-    reservation_btn = _wait(10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#nextTicketSelection')))
-    reservation_btn.click()
-
-    _wait(10).until(EC.alert_is_present())
-    macro_driver.switch_to.alert.accept()
-
-    # EXIT PROGRAM #
-    _exit('Successfully End At {}'.format(datetime.now()))
-
-# print('captcha broke')
-except Exception as EXC:
-    if EXC is KeyboardInterrupt:
-        raise Exception('==========================KeyboardInterrupt=========================')
-    else:
-        raise Exception(EXC)
-    _exit('End At {}'.format(datetime.now()))
-    
+finally:
+    print('EndTime: {}      RunTime: {}'.format(datetime.now(), datetime.now()-start_time))
+    sys_exit(0)
